4029209b54cbbdbc40821100af93d7bac1378e58
Moa Björkman <moaviola1@hotmail.com>
2020-02-24 15:01:56 +0100

    started with main method

--- src/BankApplication.java
+++ src/BankApplication.java
@@ -1,14 +1,57 @@
+import java.util.Scanner;
 
 public class BankApplication {
+	Scanner scan = new Scanner(System.in);
+	Bank bank = new Bank();
+
 	public static void main(String[] args) {
+	
 
 	}
 
 	public void runApplication() {
+		int input = scan.nextInt();
+		System.out.println("Val: " + input);
+		
+		switch (input) {			
+		case 1:
+		case 2:
+		case 3:
+			System.out.println("Från konto: ");
+			int nbr = scan.nextInt();
+			System.out.println("Belopp: ");
+			double deposit = scan.nextDouble(); //inte klart :)
+			
+			
+		case 4:
+		case 5:
+		case 6: 
+			String name = scan.nextLine();
+			long idNr = scan.nextLong();
+			bank.addAccount(name, idNr);
+		case 7:
+		case 8: 
+		case 9:
+		default:
+			System.out.println("Inget menyval med det numret. Testa något i listan istället!");
+			
+		}
+	
+		
 
 	}
 
-	public void meny() {
+	public void writeMenu() {
+
+		System.out.println("1. Hitta konto utifrån innehavare");
+		System.out.println("2. Sök kontoinnehavare utifrån (del av) namn");
+		System.out.println("3. Sätt in");
+		System.out.println("4. Ta ut");
+		System.out.println("5. Överföring");
+		System.out.println("6. Skapa konto");
+		System.out.println("7. Ta bort konto");
+		System.out.println("8. Skriv ut konton");
+		System.out.println("9. Avsluta");
 
 	}
 }
--- src/Customer.java
+++ src/Customer.java
@@ -1,33 +1,37 @@
 
-
 public class Customer {
 	private String name;
 	private long idNr;
 	private int customerNr;
-	
+
 	/**
-	* Skapar en kund (kontoinnehavare) med namnet ’name’ och id-nummer ’idNr’.
-	* Kunden tilldelas också ett unikt kundnummer.
-	*/
-	Customer(String name, long idNr){
+	 * Skapar en kund (kontoinnehavare) med namnet ’name’ och id-nummer ’idNr’.
+	 * Kunden tilldelas också ett unikt kundnummer.
+	 */
+	Customer(String name, long idNr) {
 		this.name = name;
 		this.idNr = idNr;
 		customerNr = 1000; /* FIXA! ska tänka igenom d här ordentligt sen */
 	}
+
 	/** Tar reda på kundens namn. */
 	String getName() {
-	return name;	
+		return name;
 	}
+
 	/** Tar reda på kundens personnummer. */
 	long getIdNr() {
 		return idNr;
 	}
+
 	/** Tar reda på kundens kundnummer. */
 	int getCustomerNr() {
 		return customerNr;
 	}
+
 	/** Returnerar en strängbeskrivning av kunden. */
 	public String toString() {
-		return "name: " + name + "\n" + "id: " + idNr + "\n" + "kundnr: " + customerNr;
+		String output = "name: " + name + "," + " id: " + idNr + "," + " kundnr: " + customerNr;
+		return output;
 	}
 }
