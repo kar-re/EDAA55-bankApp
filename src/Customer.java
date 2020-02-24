b9a4112c25b3d8ccd2ba8220f41a13524e243192
Moa Björkman <moaviola1@hotmail.com>
2020-02-24 13:30:08 +0100

    added skeleton for customer

--- src/Customer.java
+++ src/Customer.java
@@ -1,4 +1,24 @@
 
 public class Customer {
-
+	/**
+	* Skapar en kund (kontoinnehavare) med namnet ’name’ och id-nummer ’idNr’.
+	* Kunden tilldelas också ett unikt kundnummer.
+	*/
+	Customer(String name, long idNr){}
+	/** Tar reda på kundens namn. */
+	String getName() {
+		
+	}
+	/** Tar reda på kundens personnummer. */
+	long getIdNr() {
+		
+	}
+	/** Tar reda på kundens kundnummer. */
+	int getCustomerNr() {
+		
+	}
+	/** Returnerar en strängbeskrivning av kunden. */
+	String toString() {
+		
+	}
 }
